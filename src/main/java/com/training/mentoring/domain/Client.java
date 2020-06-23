e5f552827e8d57272ba4c8191a0d44ee14e95378
cin_rmoreira <cin_rmoreira@uolinc.com>
2020-06-23 18:52:11 -0300

    Create entity Client.

---
+++ src/main/java/com/training/mentoring/domain/Client.java
@@ -0,0 +1,12 @@
+package com.training.mentoring.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Client {
+
+    private Long id;
+    private String name;
+    private Integer age;
+
+}
